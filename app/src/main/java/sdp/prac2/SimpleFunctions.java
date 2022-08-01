8096e5e9ea320939f974c552a8f9bc294ece0a97
AgathaW <g18w7553@campus.ru.ac.za>
2022-08-01 14:48:07 +0200

    Task1

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -3,5 +3,32 @@ package sdp.prac2;
 import java.util.*;
 
 public class SimpleFunctions {
-    public SimpleFunctions() {}
+    public SimpleFunctions() {
+    }
+
+    public static int Task1(List<Integer> a, List<Integer> b) {
+        List<Integer> newList = new ArrayList();
+        int sum = 0;
+        for (int i = 0; i < a.size(); i++) {
+            int indexb = b.indexOf(i);
+            if (a.contains(indexb)) {
+                newList.add(indexb);
+                for (int j = 0; j < newList.size(); j++) {
+                    sum += j;
+                    System.out.println(sum);
+                }
+            }
+
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> ListA = Arrays.asList(1, 2, 3, 4);
+        List<Integer> ListB = Arrays.asList(4, 5, 6, 8);
+        // Task1();
+        Task1(ListA, ListB);
+
+    }
+
 }
